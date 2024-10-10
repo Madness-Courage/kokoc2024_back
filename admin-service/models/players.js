18590834a718477a81edc6af3c8d7b1c070d4b2e
N0tTryHard <billy.bones1@mail.ru>
2024-10-10 23:33:01 +0300

    player editing update

--- admin-service/models/players.js
+++ admin-service/models/players.js
@@ -12,19 +12,19 @@ class Player {
     }
 
     static async createPlayer(player) {
-        const {photo, full_name, date_of_birth, biography, position} = player;
+        const {photo, full_name, date_of_birth, biography, position, number} = player;
         const result = await teamPool.query(
-            'INSERT INTO players (photo, full_name, date_of_birth, biography, position) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-            [photo, full_name, date_of_birth, biography, position]
+            'INSERT INTO players (photo, full_name, date_of_birth, biography, position, number) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+            [photo, full_name, date_of_birth, biography, position, number]
         );
         return result.rows;
     }
 
     static async updatePlayer(id, player) {
-        const {photo, full_name, date_of_birth, biography} = player;
+        const {photo, full_name, date_of_birth, biography, position, number} = player;
         const result = await teamPool.query(
-            'UPDATE players SET photo = $1, full_name = $2, date_of_birth = $3, biography = $4 WHERE id = $5 RETURNING *',
-            [photo, full_name, date_of_birth, biography, id]
+            'UPDATE players SET photo = COALESCE($1, photo), full_name = COALESCE($2, full_name), date_of_birth = COALESCE($3, date_of_birth), biography = COALESCE($4, biography), position = COALESCE($5, position), number = COALESCE($6, number) WHERE id = $7 RETURNING *',
+            [photo, full_name, date_of_birth, biography, position, number, id]
         );
         return result.rows;
     }
