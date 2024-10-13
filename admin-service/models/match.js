389746699f9bb2d9bb5e62b05b35bcb5ee78d28d
N0tTryHard <billy.bones1@mail.ru>
2024-10-13 20:26:26 +0300

    added info about active matches

--- admin-service/models/match.js
+++ admin-service/models/match.js
@@ -12,11 +12,12 @@ class Match {
             stadium,
             start_time,
             stream_url,
-            registration_form_url
+            registration_form_url,
+            is_active
         } = match;
         const result = await matchesPool.query(
-            'INSERT INTO matches (team1, team1_logo_url, team2, team2_logo_url, team1_goals, team2_goals, stadium, start_time, stream_url, registration_form_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *',
-            [team1, team1_logo_url, team2, team2_logo_url, team1_goals, team2_goals, stadium, start_time, stream_url, registration_form_url]
+            'INSERT INTO matches (team1, team1_logo_url, team2, team2_logo_url, team1_goals, team2_goals, stadium, start_time, stream_url, registration_form_url, is_active) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *',
+            [team1, team1_logo_url, team2, team2_logo_url, team1_goals, team2_goals, stadium, start_time, stream_url, registration_form_url, is_active]
         );
         return result.rows[0];
     }
@@ -32,11 +33,12 @@ class Match {
             stadium,
             start_time,
             stream_url,
-            registration_form_url
+            registration_form_url,
+            is_active
         } = match;
         const result = await matchesPool.query(
-            'UPDATE matches SET team1 = coalesce($1, team1), team1_logo_url = coalesce($2, team1_logo_url), team2 = coalesce($3, team2), team2_logo_url = coalesce($4, team2_logo_url), team1_goals = coalesce($5, team1_goals), team2_goals = coalesce($6, team2_goals), stadium = coalesce($7, stadium), start_time = coalesce($8, start_time), stream_url = coalesce($9, stream_url), registration_form_url = coalesce($10, registration_form_url) WHERE id = $11 RETURNING *',
-            [team1, team1_logo_url, team2, team2_logo_url, team1_goals, team2_goals, stadium, start_time, stream_url, registration_form_url, id]
+            'UPDATE matches SET team1 = coalesce($1, team1), team1_logo_url = coalesce($2, team1_logo_url), team2 = coalesce($3, team2), team2_logo_url = coalesce($4, team2_logo_url), team1_goals = coalesce($5, team1_goals), team2_goals = coalesce($6, team2_goals), stadium = coalesce($7, stadium), start_time = coalesce($8, start_time), stream_url = coalesce($9, stream_url), registration_form_url = coalesce($10, registration_form_url), is_active=coalesce($11, is_active) WHERE id = $12 RETURNING *',
+            [team1, team1_logo_url, team2, team2_logo_url, team1_goals, team2_goals, stadium, start_time, stream_url, registration_form_url, is_active, id]
         );
         return result.rows[0];
     }
